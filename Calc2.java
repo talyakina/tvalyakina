341bf1f94e303928db2af1314e808ef5b5416296
talyakina <talyakina95@mail.ru>
2017-03-22 20:17:16 +0300

    Add files via upload

---
+++ Calc2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calc2 {
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 1; i++) {
+			Scanner in = new Scanner(System.in);
+			System.out.println("¬ведите число");
+			int num1 = in.nextInt();
+			System.out.println("¬ведите степень, в которую необходимо возвести число");
+			int num2 = in.nextInt();
+			int result = (int) Math.pow(num1, num2);
+			System.out.println("–езультат возведени€ в степень: " + result);
+		}
+
+	}
+}
